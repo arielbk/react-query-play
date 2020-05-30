04854d5988cdd816ca22430345030ffaa35e11d2
arielbk <buchwald.kearney@gmail.com>
2020-05-30 21:45:57 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { ReactQueryDevtools } from 'react-query-devtools';
+import { useQuery } from 'react-query';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Exchange />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </>
   );
 }
 
-export default App;
+const fetchExchange = async (currency) => {
+  const response = await fetch(` https://api.ratesapi.io/api/latest?base=${currency}`);
+  const data = await response.json();
+  return data;
+}
+
+function Exchange() {
+  const [currency, setCurrency] = useState('HRK');
+  const {status, data, error} = useQuery(currency, fetchExchange);
+
+  if (status === 'loading') return <div>Loading...</div>;
+  if (status === 'error') return <div>error: {JSON.stringify(error)}</div>
+
+  return (
+    <div>
+      <button onClick={() => setCurrency('HRK')}>HRK</button>
+      <button onClick={() => setCurrency('USD')}>USD</button>
+      <button onClick={() => setCurrency('EUR')}>EUR</button>
+      <h2>Showing currency: {currency}</h2>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+}
